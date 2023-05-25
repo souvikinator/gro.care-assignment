208fdb76dd42a986debcaeda9ebedb70fea696a1
Souvik Kar Mahapatra <souvikat001@gmail.com>
2023-05-25 06:39:20 +0530

    added description

---
+++ src/components/VideoFooter.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+type T_props = {
+  profileImage: string;
+  title: string;
+  creator: string;
+  description: string;
+};
+
+function VideoFooter(props: T_props) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <div className="absolute bottom-0 left-0 right-0 flex flex-row w-full p-4 bg-gradient-to-t  from-black">
+      <div className="w-1/12 py-2">
+        <img
+          alt="Placeholder"
+          className="block rounded-full w-11"
+          src={props.profileImage}
+        />
+      </div>
+      <div className="ml-3 w-10/12 text-white">
+        <p className="text-xl font-semibold">{props.title}</p>
+        <p className="text-md ">{props.creator}</p>
+        <div className={``} onClick={() => setExpanded((prev) => !prev)}>
+          <p className={`flex ${!expanded && "truncate"}`}>
+            {props.description}
+          </p>
+          {!expanded && <p className="font-semibold">Read more</p>}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoFooter;
